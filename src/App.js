5ca33bc32fbe5b86ebdeb32a413a1d11739c1503
pngo12 <pngo12@gmail.com>
2019-01-28 11:46:30 -0800

    first draft of how the page should look with materialui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import MainMap from './components/MainMap';
+import ResponsiveDrawer from './components/Drawer'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <MainMap />
+        <ResponsiveDrawer />
       </div>
     );
   }
--- src/components/MainMap.js
+++ src/components/MainMap.js
@@ -6,8 +6,12 @@ class MainMap extends Component {
     render() {
         return (
             <div>
-                <Map google={this.props.google} zoom={14}>
-
+                <Map 
+                google={this.props.google} zoom={14}
+                initialCenter={{
+                    lat: 33.717472,
+                    lng: -117.831146
+                }}>
                     <Marker 
                     name={'Steelhead Coffee'}
                     position={{ lat: 33.818908, lng: -118.187207}}
@@ -16,7 +20,6 @@ class MainMap extends Component {
                     name={'Bad Coffee'}
                     position={{ lat: 33.675577, lng: -117.911753}}
                     />
-
                 </Map>
             </div>
         );
